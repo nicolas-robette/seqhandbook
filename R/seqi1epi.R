53b2c723ebcea07be75827eb8a8db8f37769228d
nicolas-robette <40861887+nicolas-robette@users.noreply.github.com>
2025-06-02 20:18:26 +0000

    Delete R directory

--- R/seqi1epi.R
+++
@@ -1,6 +0,0 @@
-seqi1epi <- function(seqdata) {
-  dur <- suppressMessages(TraMineR::seqistatd(seqdata))
-  epi <- ceiling(dur/ncol(seqdata))
-  colnames(epi) <- paste0("epi", alphabet(seqdata))
-  return(epi)
-}
--- R/seqinepi.R
+++
@@ -1,6 +0,0 @@
-seqinepi <- function(seqdata) {
-  z <- suppressMessages(seqdef(seqdss(seqdata),missing="%",alphabet=alphabet(seqdata)))
-  nbepi <- suppressMessages(seqistatd(z))
-  colnames(nbepi) <- paste0("nepi", alphabet(seqdata))
-  return(nbepi)
-}
